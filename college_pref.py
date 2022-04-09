e3f6dc3fa4be78f7c4a822474167a200304338b3
hwallace17 <102485463+hwallace17@users.noreply.github.com>
2022-04-09 16:43:48 -0400

    Create college_pref.py
    Some starter code, importing csv file into dictionary and asking user preferences.

---
+++ college_pref.py
@@ -0,0 +1,33 @@
+import csv
+
+colleges = {}
+
+def load_data(directory='small'):
+    """
+    Load data from CSV files into colleges dictionary
+    """
+
+    # Load college stats
+    with open(f"{directory}/College_Data.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            name = int(row["College Name"])
+            colleges[id] = {
+                "F.undergrad": row["F.Undergrad"],
+                "S.F.Ratio": row["S.F.Ratio"],
+            }
+            
+# collect preferences from user
+
+pop_pref = input('Do you prefer a small, medium or large school population?").lower
+pop_size_list = ['small', 'medium', 'large']
+
+if pop_pref not in pop_size_list:
+    print('Size must be small, medium or large')
+else:
+    break
+
+x = colleges.get(pop_pref)
+
+print(x)
+
