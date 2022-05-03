45f1865b432046c734bb8c1bad077348e51f1b9a
hunterheadshark <103336324+hunterheadshark@users.noreply.github.com>
2022-05-03 12:02:39 -0400

    Update college_pref.py

--- college_pref.py
+++ college_pref.py
@@ -1,10 +1,6 @@
-# from code import interact
-
 import csv
 
 import statistics
-
-# from collections import Counter
 
 colleges = {}
 
@@ -16,18 +12,6 @@ def convert_to_size(population):
         return "small"
     else:
         return "medium"
-
-
-# def fix_empty_SAT(score):
-#     score = int(float(score))
-#     score = float(400 if score is None else score)
-#     # while True:
-    #     try:
-    #         score = float(score)
-    #         break
-    #     except:
-    #         score = float(400 if score is None else score)
- 
 
 def round_total_cost(cost):
     cost = int(cost/1000)
@@ -54,29 +38,8 @@ def load_data():
     return colleges
 
 
-
 # collect preferences from user
 
-        
-
-
-                 
-# TEST CODE (small dictionary)                
-# print("Get ready to get matched \U0001F60E")
-# colleges = {
-#   'Harvard': "medium",
-#   'Yale': "small"
-# }
-
-
-# # function to return key for any value
-# def get_key(val):
-#     for key, value in colleges.items():
-#          if val == value:
-#              return key
-#          else:
-#             return "not found"
-    
 # finding size preferences
 
 def ideal_size(size_pref):
@@ -86,7 +49,6 @@ def ideal_size(size_pref):
         if size_pref == str(size):
             sizes_list.append(college_name)
     return sizes_list
-    # print(sizes_list)
 
 # finding public vs private preferences
 
@@ -97,7 +59,6 @@ def ideal_type(pub_pref):
         if pub_pref == str(pref):
             pub_list.append(college_name)
     return pub_list
-    # print(pub_list)
 
 # finding cost preferences
 
@@ -112,7 +73,6 @@ def closest_price(cost_pref):
             price_list.append(college_name)
     
     return price_list
-    # print(price_list)
 
 # finding SAT matches
 def closest_score(score_pref):
@@ -129,23 +89,7 @@ def closest_score(score_pref):
             SAT_scores.append(college_name)
             
     return SAT_scores
-    # print(SAT_scores)
-
-
-
-
-# tally up the number of times a college is in the match list                 
-# top_match = statistics.mode(matches)
-
-# matches.pop(top_match)
-                  
-# second_top_match = statistics.mode(matches)
-                  
-# matches.pop(second_top_match)
-
-# third_top_match = statistics.mode(matches)
-                  
-
+    
 
 def main():
     print('Welcome to the COLLEGE MATCHMAKER \n Get ready to get matched! \U0001F60E')
@@ -153,8 +97,8 @@ def main():
     print('Loading colleges...')
     
     load_data()
-    # print(colleges)
-    ### continue with rest of main statements
+   
+    ### gather user input and check valid input
     while True:
         size_pref = input('Do you prefer a small, medium or large school population? ').lower()
         size_list = ['small', 'medium', 'large']
@@ -189,63 +133,52 @@ def main():
             print('Tuition must be a whole number. Do not include currency signs. Try again...')
         break
           
-    # key_list = list(colleges.keys())
-    # val_list = list(colleges.values())
-    matches = []
-    #print('\n', ideal_size(size_pref), '\n', ideal_type(pub_pref), '\n', closest_price(cost_pref), '\n', closest_score(SAT_score))
-    
-    #print(ideal_size(size_pref))
-    size_match = ideal_size(size_pref)
 
-    # matches.append(size_match)
+    matches = []
+
+    # add all matches from preferences list to one big list
+    
+    size_match = ideal_size(size_pref)
 
     matches.extend(size_match)
 
-    #print(matches)
-    
     pub_match = ideal_type(pub_pref)
 
-    # matches.append(pub_match)
-
     matches.extend(pub_match)
-    #print(matches)
+
     cost_match = closest_price(cost_pref)
 
-    # matches.append(cost_match)
 
     matches.extend(cost_match)
     
     SAT_match = closest_score(SAT_score)
 
-    # matches.append(SAT_match)
 
     matches.extend(SAT_match)
 
     top_match = statistics.mode(matches)
-
-    #print(matches)
-
-    print(f'top match is {top_match}')
     
-    # total_counts = Counter()
-    
-    # for school in matches:
-    #     total_counts[school] += 1
-
-    # top_matches = total_counts.most_common(3)
-
+ 
     print(f' According to our analysis, your top match is {top_match}!!')
 
-    while True:
-        see_second = input('Do you wish to see your next match? ')
 
+    def check_input(prompt):
+        answers = ['yes', 'no']
+        while prompt not in answers:
+            prompt = input('Your response must be yes or no: ')
+        return prompt
+            
+            
+    while True:
+        see_second = check_input(input('Do you wish to see your next match? '))
+        
         if see_second == 'yes':
             matches.remove(top_match)
             second_top_match = statistics.mode(matches)
             print(f' Your second top match is {second_top_match}!')
-            see_third = input('Do you wish to see your next match? ')
-
+           
             while True:
+                see_third = check_input(input('Do you wish to see your next match? '))
 
                 if see_third == 'yes':
                     matches.remove(second_top_match)
@@ -253,14 +186,11 @@ def main():
                     print(f' Your third top match is {third_top_match}! \n You\'re all done!')
                 elif see_third == 'no':
                     print('You\'re all done!')
-                else:
-                    print('Sorry, we didn\'t understand your response. Please respond with yes or no.')
-                break
+                    break
             
         elif see_second == 'no':
             print('You\'re all done!')
-        else:
-            print('Sorry, we didn\'t understand your response. Please respond with yes or no.')
+
         break
 if __name__ == '__main__':
     main()
